facc692325d40c9c20485d0083471fb4a58c5fe9
RobertDruska <robert.druska@gmail.com>
2020-11-20 15:51:04 +0100

    init

---
+++ src/components/FacebookShareButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <ShareNetwork
+    network="facebook"
+    url="https://druskacik.sk/"
+    title="testujeme facebook share"
+    description="popisok nejaký."
+    quote="neviem čo toto chce byť"
+    hashtags="uhlikovastopa"
+  >
+    Share on Facebook
+</ShareNetwork>
+</template>
+
+<script>
+
+export default {
+  name: 'facebook-share-button',
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div class="share-button-container">
+      <facebook-share-button />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import FacebookShareButton from '../components/FacebookShareButton.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    FacebookShareButton,
+  }
+}
+</script>
+
+<style scoped>
+
+.share-button-container {
+  padding-top: 4rem;
+}
+
+</style>
